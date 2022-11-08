0b03e25d42a58b422058a5eed84b3930cdbdfaf2
Dustin Vidrine <Dstnvidrn@gmail.com>
2022-11-07 20:01:52 -0600

    Add tab features and animation on click

--- script.js
+++ script.js
@@ -51,8 +51,23 @@ document.querySelector('.nav__links').addEventListener('click', function(e) {
     }
 })
 
-const h1 = document.querySelector('h1');
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
 
+// tabs.forEach( el => el.addEventListener('click', function(e) {console.log('TAB')}));
+tabsContainer.addEventListener('click', function(e){
+    const clicked = e.target.closest('.operations__tab');
+
+    if(!clicked) return;
+    tabs.forEach( ele => ele.classList.remove('operations__tab--active'))
+    tabsContent.forEach( content => content.classList.remove('operations__content--active'))
+    clicked.classList.add('operations__tab--active');
+    document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add('operations__content--active');
+    
+    
+
+})
 
 
 
